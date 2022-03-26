c33c0978eb19dbb214895ef2d30d778ff9807009
Antonina Listopadova <tommiante@gmail.com>
2022-03-26 21:42:02 +0300

    Add inputs validation

--- app/js/form.js
+++ app/js/form.js
@@ -1,11 +1,38 @@
 export function getSize() {
-  return document.querySelector('#size').value;
+  const min = 3;
+  const max = 1000;
+  let size = document.querySelector('#size').value;
+
+  if (size < min) size = min;
+  if (size > max) size = max;
+
+  document.querySelector('#size').value = size;
+  
+  return size;
 }
 
 export function getIterationCount() {
-  return document.querySelector('#count').value;
+  const min = 2;
+  const max = 5000;
+  let count = document.querySelector('#count').value;
+
+  if (count < min) count = min;
+  if (count > max) count = max;
+
+  document.querySelector('#count').value = count;
+
+  return count;
 }
 
 export function getTimeInterval() {
-  return document.querySelector('#time').value;
+  const min = 0.01;
+  const max = 60;
+  let time = document.querySelector('#time').value;
+
+  if (time < min) time = min;
+  if (time > max) time = max;
+
+  document.querySelector('#time').value = time;
+
+  return time;
 }
