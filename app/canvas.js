49d1f525cc26c68967aa1647f391c5dfe3b353e2
Antonina Listopadova <tommiante@gmail.com>
2022-03-26 15:37:51 +0300

    Add visualization of game and TODOs

---
+++ app/canvas.js
@@ -0,0 +1,61 @@
+const SIZE = 300;
+
+const VIOLET = '#6A0AAB';
+const GRAY = '#d5d5d5';
+const GRAYLIGHT = '#eee';
+const WHITE = '#fff';
+
+// TODO: make it work for non-square matrix (and beautiful)
+
+export function drawCells(context, matrix, x, y) {
+  const coef = SIZE / x;
+
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < y; j++) {
+      const width = coef - 2;
+      const height = coef - 2;
+
+      if (matrix[j][i]) {
+        context.fillStyle = VIOLET;
+        context.fillRect(i * coef + 1, j * coef + 1, width, height);
+      } else {
+        context.fillStyle = WHITE;
+        context.fillRect(i * coef + 1, j * coef + 1, width, height);
+      }
+    }
+  }
+}
+
+export function drawGrid(context, x, y) {
+  const coef = SIZE / x;
+
+  context.beginPath();
+
+  for (let i = 1; i < x; i++) {
+    context.moveTo(i * coef, 0);
+    context.lineTo(i * coef, SIZE);
+  }
+  
+  for (let j = 1; j < y; j++) {
+    context.moveTo(0, j * coef);
+    context.lineTo(SIZE, j * coef);
+  }
+  
+  context.strokeStyle = GRAYLIGHT;
+  context.stroke();
+
+  drawFieldBorder(context);
+}
+
+function drawFieldBorder(context){
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(0, SIZE);
+  context.lineTo(SIZE, SIZE);
+  context.lineTo(SIZE, 0);
+  context.lineTo(0, 0);
+
+  context.lineWidth = 2;
+  context.strokeStyle = GRAY;
+  context.stroke();
+}
--- app/index.js
+++ app/index.js
@@ -1,1 +1,35 @@
+import { generateRandomMatrix,
+         calcField } from './life';
+import { drawGrid,
+         drawCells } from './canvas';
+
 import './styles/index.styl';
+
+const canvas = document.getElementById('life');
+const context = canvas.getContext('2d');
+
+// TODO: add inputs for customize:
+const defaultSize = 20;
+const defaultIterationConut = 10;
+const defaultTimeInterval = 1;
+
+function startGameOfLife(iterationCount, timeIntervalSec) {
+  let matrix = generateRandomMatrix(defaultSize, defaultSize);
+
+  drawGrid(context, defaultSize, defaultSize);
+
+  function makeIteration() {
+    matrix = calcField(matrix);
+
+    drawCells(context, matrix, defaultSize, defaultSize);
+
+    if (iterationCount) {
+      iterationCount--;
+      setTimeout(makeIteration, timeIntervalSec * 1000);
+    }
+  }
+
+  makeIteration();
+}
+
+startGameOfLife(defaultIterationConut, defaultTimeInterval);
