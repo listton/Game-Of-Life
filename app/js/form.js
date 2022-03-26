8e2302ac41263d0ca4a1387bc2c901189272a6bf
Antonina Listopadova <tommiante@gmail.com>
2022-03-26 17:28:26 +0300

    Add form for customize parameters

---
+++ app/js/form.js
@@ -0,0 +1,11 @@
+export function getSize() {
+  return document.querySelector('#size').value;
+}
+
+export function getIterationCount() {
+  return document.querySelector('#count').value;
+}
+
+export function getTimeInterval() {
+  return document.querySelector('#time').value;
+}
